230adcd49af8ab8276c49471d2ea6af9b02eb37a
[K]ONGOR <project.kongor@proton.me>
2025-10-27 21:54:37 +0000

    Checkpoint For Clean-Up

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,4 +1,4 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
@@ -11,38 +11,68 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Lorem Ipsum Dolor',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+        doloremque laudantium totam rem aperiam.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Consectetur Adipiscing',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+        sed quia consequuntur magni dolores eos qui ratione.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Tempor Incididunt',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Ut enim ad minima veniam quis nostrum exercitationem ullam corporis suscipit
+        laboriosam nisi ut aliquid ex ea commodi.
       </>
     ),
   },
+  {
+    title: 'Magna Aliqua',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
+        nihil molestiae consequatur vel illum qui dolorem.
+      </>
+    ),
+  },
+  {
+    title: 'Eiusmod Labore',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+        praesentium voluptatum deleniti atque corrupti.
+      </>
+    ),
+  },
+  {
+    title: 'Fugiat Nulla',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Et harum quidem rerum facilis est et expedita distinctio nam libero tempore
+        cum soluta nobis est eligendi optio cumque.
+      </>
+    ),
+  }
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
